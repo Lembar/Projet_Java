d52768296749813e5735152bb43fe56a0f421380
Lembar <leo.lemetayerbaron@gmail.com>
2015-11-17 07:12:18 +0100

    EmployeModele

--- ProjCPAO/src/modele/EmployeModele.java
+++ ProjCPAO/src/modele/EmployeModele.java
@@ -1,5 +1,69 @@
 package modele;
 
-public class EmployeModele {
+import java.util.ArrayList;
+import java.util.Collections;
 
+import javax.swing.table.AbstractTableModel;
+
+import model.metier.Employe;
+
+public class EmployeModele extends AbstractTableModel {
+
+	private static final long serialVersionUID = -6727323471577554238L;
+
+	private ArrayList<Employe> listeEmp;
+
+	private final String[] COLONNES = { "Nom", "Prenom" };
+
+	public EmployeModele(ArrayList<Employe> arrayList) {
+		this.listeEmp = arrayList;
+	}
+
+	@Override
+	public int getColumnCount() {
+		return this.COLONNES.length;
+	}
+
+	@Override
+	public String getColumnName(int c) {
+		return this.COLONNES[c];
+	}
+
+	@Override
+	public int getRowCount() {
+		return this.listeEmp.size();
+	}
+
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		Employe emp = this.listeEmp.get(rowIndex);
+		Object obj = null;
+		switch (columnIndex) {
+		case 0:
+			obj = emp.getNom();
+			break;
+		case 1:
+			obj = emp.getPrenom();
+		}
+		return obj;
+	}
+
+	public Employe getEmploye(int index) {
+		return listeEmp.get(index);
+	}
+
+	public void addRow(Employe emp) {
+
+		this.listeEmp.add(emp);
+		Collections.sort(this.listeEmp);
+		this.fireTableDataChanged();
+	}
+
+	public void deleteRow(int i) {
+		this.listeEmp.remove(i);
+		Collections.sort(this.listeEmp);
+		this.fireTableDataChanged();
+	}
+	
 }
+
